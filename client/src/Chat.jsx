619ba510ac96a15a009c15736a25b81ce8816abc
Vigneshk5 <vigneshpatali5@gmail.com>
2023-05-17 18:46:37 +0530

    selecting contact

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Avatar from "./Avatar";
+import Logo from "./Logo";
+import { UserContext } from "./UserContext";
 
 export default function Chat() {
   const [ws, setWs] = useState(null);
   const [onlinePeople, setOnlinePeople] = useState({});
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const { username, id } = useContext(UserContext);
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:4000");
@@ -26,43 +30,47 @@ export default function Chat() {
     }
   }
 
+  const onlinePeopleExclOurUser = { ...onlinePeople };
+  delete onlinePeopleExclOurUser[id];
+
   return (
     <div className="flex h-screen">
-      <div className="bg-white w-1/3 pl-4 pt-4">
-        <div className="text-indigo-600 font-bold flex gap-2 mb-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.337 21.718a6.707 6.707 0 01-.533-.074.75.75 0 01-.44-1.223 3.73 3.73 0 00.814-1.686c.023-.115-.022-.317-.254-.543C3.274 16.587 2.25 14.41 2.25 12c0-5.03 4.428-9 9.75-9s9.75 3.97 9.75 9c0 5.03-4.428 9-9.75 9-.833 0-1.643-.097-2.417-.279a6.721 6.721 0 01-4.246.997z"
-              clipRule="evenodd"
-            />
-          </svg>
-          ChatApp
-        </div>
-        {Object.keys(onlinePeople).map((userId) => (
+      <div className="bg-white w-1/3 ">
+        <Logo />
+        {Object.keys(onlinePeopleExclOurUser).map((userId) => (
           <div
+            onClick={() => setSelectedUserId(userId)}
             key={userId}
-            className="border-b border-gray-100 py-2 flex items-center gap-2 cursor-pointer"
+            className={
+              "border-b border-gray-100 flex items-center gap-2 cursor-pointer " +
+              (userId === selectedUserId ? "bg-indigo-50" : "")
+            }
           >
-            <Avatar username={onlinePeople[userId]} userId={userId} />
-            <span className="text-gray-700"> {onlinePeople[userId]}</span>
+            {userId === selectedUserId && (
+              <div className="w-1 h-12 bg-indigo-500 rounded-r-md"></div>
+            )}
+            <div className="flex gap-2  py-2 pl-4 items-center">
+              <Avatar username={onlinePeople[userId]} userId={userId} />
+              <span className="text-gray-700"> {onlinePeople[userId]}</span>
+            </div>
           </div>
         ))}
       </div>
-      <div className="flex flex-col bg-blue-50 w-2/3 p-2">
-        <div className="flex-grow">message with selected person</div>
+      <div className="flex flex-col bg-indigo-50 w-2/3 p-2">
+        <div className="flex-grow">
+          {!selectedUserId && (
+            <div className="flex  h-full flex-grow items-center justify-center">
+              <div className="text-gray-300">&larr; Select a contact</div>
+            </div>
+          )}
+        </div>
         <div className="flex gap-2 ">
           <input
             type="text"
             placeholder="Type your message here"
             className="bg-white flex-grow rounded-sm border p-2"
           />
-          <button className="bg-blue-500 p-2 rounded-lg text-white">
+          <button className="bg-indigo-500 p-2 rounded-lg text-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
