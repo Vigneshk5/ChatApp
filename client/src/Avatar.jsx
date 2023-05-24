6807116e994661b045ed88648384ec549b758f06
Vigneshk5 <vigneshpatali5@gmail.com>
2023-05-24 20:23:02 +0530

    online indicator & offline people

--- client/src/Avatar.jsx
+++ client/src/Avatar.jsx
@@ -1,4 +1,4 @@
-export default function Avatar({ userId, username }) {
+export default function Avatar({ userId, username, online }) {
   const colors = [
     "bg-blue-200",
     "bg-red-200",
@@ -9,8 +9,11 @@ export default function Avatar({ userId, username }) {
   ];
   const color = colors[parseInt(userId, 16) % colors.length];
   return (
-    <div className={"w-7 h-7  rounded-full  flex item-center " + color}>
+    <div className={"w-7 h-7  relative rounded-full  flex item-center " + color}>
       <div className="text-center w-full opacity-70">{username[0]}</div>
+      {online && (
+        <div className="absolute w-3 h-3 bg-green-500 bottom-0 right-0 rounded-full border border-white "></div>
+      )}
     </div>
     //.toUpperCase() to uppercase profile
   );
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -4,6 +4,7 @@ import Logo from "./Logo";
 import { UserContext } from "./UserContext";
 import { uniqBy } from "lodash";
 import axios from "axios";
+import Contact from "./Contact";
 
 export default function Chat() {
   const [ws, setWs] = useState(null);
@@ -11,6 +12,7 @@ export default function Chat() {
   const [selectedUserId, setSelectedUserId] = useState(null);
   const [newMessageText, setNewMessageText] = useState("");
   const [messages, setMessages] = useState([]);
+  const [offlinePeople, setOfflinePeople] = useState({});
   const { username, id } = useContext(UserContext);
   const divUnderMessages = useRef();
 
@@ -75,6 +77,19 @@ export default function Chat() {
   }, [messages]);
 
   useEffect(() => {
+    axios.get("/people").then((res) => {
+      const offlinePeopleArr = res.data
+        .filter((p) => p._id !== id)
+        .filter((p) => !Object.keys(onlinePeople).includes(p._id));
+      const offlinePeople = {};
+      offlinePeopleArr.forEach((p) => {
+        offlinePeople[p._id] = p;
+      });
+      setOfflinePeople(offlinePeople);
+    });
+  }, [onlinePeople]);
+
+  useEffect(() => {
     if (selectedUserId) {
       axios.get("/messages/" + selectedUserId).then((res) => {
         setMessages(res.data);
@@ -92,22 +107,27 @@ export default function Chat() {
       <div className="bg-white w-1/3 ">
         <Logo />
         {Object.keys(onlinePeopleExclOurUser).map((userId) => (
-          <div
-            onClick={() => setSelectedUserId(userId)}
+          <Contact
             key={userId}
-            className={
-              "border-b border-gray-100 flex items-center gap-2 cursor-pointer " +
-              (userId === selectedUserId ? "bg-indigo-50" : "")
-            }
-          >
-            {userId === selectedUserId && (
-              <div className="w-1 h-12 bg-indigo-500 rounded-r-md"></div>
-            )}
-            <div className="flex gap-2  py-2 pl-4 items-center">
-              <Avatar username={onlinePeople[userId]} userId={userId} />
-              <span className="text-gray-700"> {onlinePeople[userId]}</span>
-            </div>
-          </div>
+            id={userId}
+            online={true}
+            username={onlinePeopleExclOurUser[userId]}
+            onClick={() => {
+              setSelectedUserId(userId);
+              console.log({ userId });
+            }}
+            selected={userId === selectedUserId}
+          />
+        ))}
+        {Object.keys(offlinePeople).map((userId) => (
+          <Contact
+            key={userId}
+            id={userId}
+            online={false}
+            username={offlinePeople[userId].username}
+            onClick={() => setSelectedUserId(userId)}
+            selected={userId === selectedUserId}
+          />
         ))}
       </div>
       <div className="flex flex-col bg-indigo-50 w-2/3 p-2">
